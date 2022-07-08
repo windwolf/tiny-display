46c98d244eae883d826e10c90dfe1e9b2430f96f
zhoujian <zhoujian.ww@gmail.com>
2022-07-08 16:24:23 +0800

    调整 cmake 调整App 结构

--- src/App/appMain.c
+++ src/App/appMain.c
@@ -5,22 +5,23 @@
 #include "config.h"
 #include "ringbuffer.h"
 #include "common/stream.h"
+#include "message/message_parser.h"
 
 static UartDevice uart1Dev;
 extern UART_HandleTypeDef huart1;
 static RingBuffer uartRxRingBuffer;
 static Stream stream;
 static uint8_t uart1_rx_buffer[UART1_RX_BUFFER_SIZE];
-// static MessageParser mp;
-// static MessageSchema schema = {
-//     .prefixSize = 0,
-//     .mode = MESSAGE_SCHEMA_MODE_FREE_LENGTH,
-//     .cmdLength = 0,
-//     .crc.length = 0,
-//     .alterDataSize = 0,
-//     .suffixSize = 2,
-//     .suffix = {'\r', '\n'},
-// };
+static MessageParser mp;
+static MessageSchema schema = {
+    .prefixSize = 0,
+    .mode = MESSAGE_SCHEMA_MODE_FREE_LENGTH,
+    .cmdLength = 0,
+    .crc.length = 0,
+    .alterDataSize = 0,
+    .suffixSize = 2,
+    .suffix = {'\r', '\n'},
+};
 void appMain(void)
 {
   ringbuffer_create(&uartRxRingBuffer, uart1_rx_buffer, 1, UART1_RX_BUFFER_SIZE);
@@ -28,11 +29,12 @@ void appMain(void)
   uart_device_create(&uart1Dev, &huart1, 4);
 
   stream_create(&stream, &uart1Dev, &uartRxRingBuffer);
-  // message_parser_create(&mp, "demo", &schema, &uartRxRingBuffer);
+  message_parser_create(&mp, "demo", &schema, &uartRxRingBuffer);
 
   uart_device_init(&uart1Dev);
   uart_device_circular_rx_start(&uart1Dev, uart1_rx_buffer, UART1_RX_BUFFER_SIZE);
-  {
+
+    {
     ssd1306_demo();
   }
 }
