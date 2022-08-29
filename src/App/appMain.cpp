aba3ea3f6a709bb59dda0e3c86e1a98fd0b9c148
zhoujian <zhoujian.ww@gmail.com>
2022-08-29 22:53:01 +0800

    迁移到新init

--- src/App/appMain.cpp
+++ src/App/appMain.cpp
@@ -58,22 +58,14 @@ class App
 };
 
 App::App()
-    : scl(*GPIOB, GPIO_PIN_6), sda(*GPIOB, GPIO_PIN_7), softi2c(scl, sda),
-      ZJ0_91in(softi2c),
-      uartRxRingBuffer(uart1_rx_buffer, 1, UART1_RX_BUFFER_SIZE),
-      uart1Dev(huart1), stream(uart1Dev, uartRxRingBuffer),
-      mp(uartRxRingBuffer), wh(){
-
-                            };
+    : scl(*GPIOB, GPIO_PIN_6), sda(*GPIOB, GPIO_PIN_7), softi2c(scl, sda), ZJ0_91in(softi2c),
+      uartRxRingBuffer(uart1_rx_buffer, 1, UART1_RX_BUFFER_SIZE), uart1Dev(huart1),
+      stream(uart1Dev, uartRxRingBuffer), mp(uartRxRingBuffer), wh(){};
 void App::setup()
 {
     scl.config_get().inverse = false;
-    scl.init();
     sda.config_get().inverse = false;
-    sda.init();
-    softi2c.init();
-    auto &sd3306_cfg = ZJ0_91in.config_get();
-    sd3306_cfg = SSD1306Config{
+    ZJ0_91in.config_get() = SSD1306Config{
         .width = 128,
         .height = 32,
         .memoryMode = SSD1306_MEMORY_ADDRESSING_MODE_VERTICAL,
@@ -92,9 +84,7 @@ void App::setup()
         .fosc = 0x0F,
         .clkDivide = 0x00,
     };
-    ZJ0_91in.init();
-    uart1Dev.init();
-    stream.init();
+
     mp.init(MessageSchema{
         .prefix = {0xFF, 0xFE},
         .prefixSize = 2,
@@ -170,16 +160,13 @@ void App::loop()
         ZJ0_91in.clear();
 
         sprintf(str, "rssi=%d", rssi);
-        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 0, str, &Font6x8_v,
-                       &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 0, str, &Font6x8_v, &fontDrawInfo);
 
         sprintf(str, "r1=%d r2=%d", rssi1, rssi2);
-        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 8, str, &Font6x8_v,
-                       &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 8, str, &Font6x8_v, &fontDrawInfo);
 
         sprintf(str, "r3=%d r4=%d", rssi3, rssi4);
-        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 16, str, &Font6x8_v,
-                       &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 16, str, &Font6x8_v, &fontDrawInfo);
         ZJ0_91in.draw();
         LOG_I("draw finished.");
     }
