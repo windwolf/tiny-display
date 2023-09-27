231a4e3b463e697b801422ad2a92ce68169bd6bd
zhoujian <zhoujian@gmail.com>
2023-09-27 15:06:29 +0800

    按照weibotlib风格重构

--- src/App/appMain.cpp
+++ src/App/appMain.cpp
@@ -15,6 +15,7 @@
 #include "usart.h"
 #include "spi.h"
 #include "gpio.h"
+#include "stream.hpp"
 
 using namespace wibot;
 
@@ -34,11 +35,9 @@ class App : AppFramework {
     I2cMaster    softi2c;
     Ssd1306      ZJ0_91in;
 
-    uint8_t                 uart1_rx_buffer[UART1_RX_BUFFER_SIZE];
-    CircularBuffer<uint8_t> uartRxRingBuffer;
-    UART                    uart1Dev;
+    BUFFER(uartBuffer, 32);
+    UART uart1Dev;
 
-    MessageParser                  mp;
     static constexpr MessageSchema schema = {
         .prefix     = {0xFF, 0xFE},
         .prefixSize = 2,
@@ -54,7 +53,7 @@ class App : AppFramework {
         .crcSize    = DataWidth::kNone,
         .suffixSize = 0,
     };
-    WaitHandler wh;
+    Stream stream;
 };
 
 App::App()
@@ -64,10 +63,8 @@ App::App()
       sda(*GPIOB, GPIO_PIN_7),
       softi2c(scl, sda),
       ZJ0_91in(softi2c),
-      uartRxRingBuffer(uart1_rx_buffer, UART1_RX_BUFFER_SIZE),
       uart1Dev(huart1, "app"),
-      mp(schema, uartRxRingBuffer),
-      wh(){};
+      stream(uart1Dev, uartBuffer, schema){};
 void App::onSetup() {
     scl.config.inverse = false;
     sda.config.inverse = false;
@@ -110,49 +107,38 @@ void App::onSetup() {
 
     };
     ZJ0_91in.init();
-    // message_parser_test();
-    // wibot::device::demo::st77xx_demo();
-    // wibot::device::demo::w25qxx_demo();
-    uart1Dev.start(uartRxRingBuffer, wh);
+    stream.open();
 };
 
 void App::onRun() {
     LOG_I("loop begin");
-    uint16_t rssi;
-    uint16_t rssi1;
-    uint16_t rssi2;
-    uint16_t rssi3;
-    uint16_t rssi4;
-    bool     get = false;
 
     uint8_t      fData[16];
     MessageFrame frame({fData, 16});
-    wh.wait(TIMEOUT_FOREVER);
 
-    while (mp.parse(&frame) == Result::kOk) {
-        auto ctn = frame.getContent();
-        rssi     = ctn.getUint16(0);
-        rssi1    = ctn.getUint16(2);
-        rssi2    = ctn.getUint16(4);
-        rssi3    = ctn.getUint16(6);
-        rssi4    = ctn.getUint16(8);
-        get      = true;
-    };
+    while (true) {
+        auto rst = stream.readSync(frame, TIMEOUT_FOREVER);
+        if (rst == Result::kOk) {
+            auto     ctn     = frame.getContent();
+            uint16_t rssi    = ctn.getUint16(0);
+            uint16_t rssi1   = ctn.getUint16(2);
+            uint16_t rssi2   = ctn.getUint16(4);
+            uint16_t rssi3   = ctn.getUint16(6);
+            uint16_t rssi4   = ctn.getUint16(8);
+            char     str[22] = {0};
+            ZJ0_91in.clear();
 
-    if (get) {
-        char str[22] = {0};
-        ZJ0_91in.clear();
+            sprintf(str, "rssi=%d", rssi);
+            FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 0, str, &Font6x8_v, &fontDrawInfo);
 
-        sprintf(str, "rssi=%d", rssi);
-        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 0, str, &Font6x8_v, &fontDrawInfo);
+            sprintf(str, "r1=%d r2=%d", rssi1, rssi2);
+            FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 8, str, &Font6x8_v, &fontDrawInfo);
 
-        sprintf(str, "r1=%d r2=%d", rssi1, rssi2);
-        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 8, str, &Font6x8_v, &fontDrawInfo);
-
-        sprintf(str, "r3=%d r4=%d", rssi3, rssi4);
-        FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 16, str, &Font6x8_v, &fontDrawInfo);
-        ZJ0_91in.draw();
-        LOG_I("draw finished.");
+            sprintf(str, "r3=%d r4=%d", rssi3, rssi4);
+            FONTS_FillData(ZJ0_91in.dataBuffer, &canvasInfo, 0, 16, str, &Font6x8_v, &fontDrawInfo);
+            ZJ0_91in.draw();
+            LOG_I("draw finished.");
+        }
     }
 };
 static App app1 = App();
